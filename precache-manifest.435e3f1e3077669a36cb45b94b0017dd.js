4d6bed52d8f00c1562248d8c10992a9ab2db9a86
Radoslaw Kolodziejczyk <radek940501@interia.pl>
2019-08-26 21:35:36 +0200

    Updates

---
+++ precache-manifest.435e3f1e3077669a36cb45b94b0017dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "86a7f19c4e18a138b061fe3bb57802f6",
+    "url": "/amount-in-words/static/media/bg_money.86a7f19c.jpg"
+  },
+  {
+    "revision": "145fcbeb2f7640d5564d",
+    "url": "/amount-in-words/static/js/runtime~main.dd90666e.js"
+  },
+  {
+    "revision": "6919970594ea543be9a1",
+    "url": "/amount-in-words/static/js/main.04c4b34e.chunk.js"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/amount-in-words/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "6919970594ea543be9a1",
+    "url": "/amount-in-words/static/css/main.4bb6fc88.chunk.css"
+  },
+  {
+    "revision": "263a40be28b3a9e74b2453ae1bac68e7",
+    "url": "/amount-in-words/index.html"
+  }
+];
